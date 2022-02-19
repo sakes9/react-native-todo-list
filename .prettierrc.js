219c677f0c13385510973b69a612db97443a5a33
sakes9 <sake-std-co@gmail.com>
2022-02-19 22:02:32 +0900

    【アプリ】プロジェクトの初期設定　テキストエディタの設定
    プロジェクトのコードスタイル設定

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // 文末のカンマ設定
+  //  none: カンマをつけない（デフォルト）
+  //  es5:  一部にカンマをつける
+  //  all:  全てにカンマをつける
+  trailingComma: 'es5',
+  // 行の折り返し長さ（デフォルト: 80）
+  printWidth: 150,
+  // インデントのスペース数（デフォルト: 2）
+  tabWidth: 2,
+  // シングルクォート（デフォルト: true）
+  singleQuote: true,
+  // 文末にセミコロンを追加（デフォルト: true）
+  semi: true,
+  // JSX要素の文末に「>」を置く（デフォルト: false）
+  jsxBracketSameLine: true,
+};
