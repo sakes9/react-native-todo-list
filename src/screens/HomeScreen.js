7011d64460f0f7f8bf92c0a8628bfb2fe467bfeb
sakes9 <sake-std-co@gmail.com>
2022-02-20 20:29:40 +0900

    【アプリ】画面作成　画面遷移を実装する

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,21 @@
+import { useLayoutEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }) {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <TouchableOpacity style={{ marginRight: 15 }} onPress={() => navigation.navigate('Tab')}>
+            <Icon name="folder" color={'white'} size={25}></Icon>
+          </TouchableOpacity>
+        );
+      },
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>ホーム画面</Text>
