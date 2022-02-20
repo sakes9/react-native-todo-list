d6b12d3f691af21e5c9fa90f7bf8d62a64da8384
sakes9 <sake-std-co@gmail.com>
2022-02-20 20:29:40 +0900

    【アプリ】画面作成　画面遷移を実装する

--- src/screens/TabScreen.js
+++ src/screens/TabScreen.js
@@ -1,7 +1,21 @@
+import { useLayoutEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default function TabScreen() {
+export default function TabScreen({ navigation }) {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <TouchableOpacity style={{ marginRight: 15 }}>
+            <Icon name="plus" color={'white'} size={25}></Icon>
+          </TouchableOpacity>
+        );
+      },
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>タブ管理画面</Text>
