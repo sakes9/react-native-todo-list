03dbe08c279e5995e0fd2fec3be6d4f48684697c
sakes9 <sake-std-co@gmail.com>
2022-02-20 16:33:22 +0900

    【アプリ】画面作成　ホーム画面を作成する

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>ホーム画面</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F2F2F7',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
