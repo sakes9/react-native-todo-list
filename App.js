dcfe4db4353e17c91fea8deede0e824af69327e9
sakes9 <sake-std-co@gmail.com>
2022-02-20 16:42:24 +0900

    【アプリ】画面作成　タブ管理画面を作成する

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
+import TabScreen from './src/screens/TabScreen';
 
 export default function App() {
-  return <HomeScreen></HomeScreen>;
+  return <TabScreen></TabScreen>;
 }
