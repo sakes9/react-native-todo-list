abb1f488d1976d54c41ec21112b9cd1efa50f27c
sakes9 <sake-std-co@gmail.com>
2022-02-20 19:10:06 +0900

    【アプリ】画面作成　ナビゲーション設定を行う

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 import TabScreen from './src/screens/TabScreen';
+import RootNavigation from './src/navigations/RootNavigation';
 
 export default function App() {
-  return <TabScreen></TabScreen>;
+  return <RootNavigation></RootNavigation>;
 }
