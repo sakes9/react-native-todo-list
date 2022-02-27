242acb327d0a221332c33f15d65ed5fc82fc0a87
sakes9 <sake-std-co@gmail.com>
2022-02-27 20:35:31 +0900

    【アプリ】UI作成　タブビューを作成する

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,37 @@
-import { useLayoutEffect } from 'react';
+import React, { useLayoutEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, useWindowDimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Checkmark from '../components/Checkmark';
 import TodoListItem from '../components/TodoListItem';
 import FloatingButton from '../components/FloatingButton';
 import AlertUtil from '../utils/AlertUtil';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+
+const FirstRoute = () => <View style={{ flex: 1, backgroundColor: '#ff4081' }} />;
+
+const SecondRoute = () => <View style={{ flex: 1, backgroundColor: '#673ab7' }} />;
+
+const renderScene = SceneMap({
+  tab1: FirstRoute,
+  tab2: SecondRoute,
+  tab3: FirstRoute,
+  tab4: SecondRoute,
+  tab5: FirstRoute,
+});
 
 export default function HomeScreen({ navigation }) {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'tab1', title: 'タブ1' },
+    { key: 'tab2', title: 'タブ2' },
+    { key: 'tab3', title: 'タブ3' },
+    { key: 'tab4', title: 'タブ4' },
+    { key: 'tab5', title: 'タブ5' },
+  ]);
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => {
@@ -28,11 +52,34 @@ export default function HomeScreen({ navigation }) {
     AlertUtil.showTextInputAlert('Todo追加', '追加するTodo名を入力してください', (text) => console.log(text));
   }
 
+  const renderTabBar = (props) => {
+    return (
+      <TabBar
+        {...props}
+        activeColor={'#167476'}
+        inactiveColor={'#505050'}
+        style={{ backgroundColor: 'white' }}
+        tabStyle={{ width: 'auto', minWidth: 100 }}
+        labelStyle={{ color: 'black', fontWeight: 'bold' }}
+        indicatorStyle={{ borderBottomWidth: 2, borderColor: '#6B7076' }}
+        scrollEnabled={true}
+        pressOpacity={1}></TabBar>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <View style={{ width: '100%' }}>
+      <TabView
+        renderTabBar={renderTabBar}
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{ width: layout.width }}
+      />
+
+      {/* <View style={{ width: '100%' }}>
         <TodoListItem todoTitle={'てすとTodo'}></TodoListItem>
-      </View>
+      </View> */}
 
       <FloatingButton onPress={showAddTodoAlert}></FloatingButton>
       <StatusBar style="light" />
@@ -44,7 +91,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F2F2F7',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
